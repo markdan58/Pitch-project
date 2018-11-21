997f0f2c360670bd0f5d9e06dcaf79a35dc37e93
markdan58 <mark.dan@strathmore.edu>
2018-11-21 10:25:09 +0300

    added 404 error page

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,15 +5,3 @@ main = Blueprint('main',__name__)
 
 from . import views,errors
 
-
-ERRORS
-
-from flask import render_template
-from . import main
-
-@main.app_errorhandler(404)
-def four_Ow_four(error):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('fourOwfour.html'),404
